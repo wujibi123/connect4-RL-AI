45f965cea9caccbd385a01d259478d88d71ae944
Dan Bi <biwuji123@gmail.com>
2018-08-16 17:00:24 -0400

    Add files via upload

---
+++ c4.py
@@ -0,0 +1,65 @@
+import numpy as np
+from mygrad import sliding_window_view
+
+def create_board(rowcount, colcount):
+    return np.zeros((rowcount, colcount))
+
+def is_valid_move(board, col, rowcount):
+    if board[0][col] == 0:
+        return True
+    else:
+        return False
+
+def get_next_open_row(board, col, rowcount):
+    for i in range(rowcount):
+        if board[i][col] != 0:
+            return i - 1
+    return rowcount - 1
+
+
+def drop_piece(board, col, rowcount, piece):
+    row = get_next_open_row(board, col, rowcount)
+    board[row][col] = piece
+    return None
+
+d1_filter = np.zeros((4, 4))
+for i in range(4):
+    d1_filter[i][i] = 1
+
+d2_filter = np.zeros((4, 4))
+for i in range(4):
+    d2_filter[-1*(i+1)][i] = 1
+
+
+def win_ver(board):
+    h = sliding_window_view(board, (1, 4), (1,1)).reshape(-1, 4)
+    for i in h:
+        c = list(i)
+        if c == [1, 1, 1, 1]:
+            return True #True = player1 wins
+        if c == [2, 2, 2, 2]:
+            return False #False = player2 wins
+    
+    v = sliding_window_view(board, (4, 1), (1,1)).reshape(-1, 4)
+    for i in v:
+        c = list(i)
+        if c == [1, 1, 1, 1]:
+            return True #True = player1 wins
+        if c == [2, 2, 2, 2]:
+            return False #False = player2 wins
+
+    d1 = np.sum(sliding_window_view(board, (4, 4), (1,1)).reshape(-1, 4, 4) * d1_filter, axis = 1, dtype=int)
+    for i in d1:
+        c = list(i)
+        if c == [1, 1, 1, 1]:
+            return True #True = player1 wins
+        if c == [2, 2, 2, 2]:
+            return False #False = player2 wins
+    
+    d2 = np.sum(sliding_window_view(board, (4, 4), (1,1)).reshape(-1, 4, 4) * d2_filter, axis = 1, dtype=int)
+    for i in d2:
+        c = list(i)
+        if c == [1, 1, 1, 1]:
+            return True #True = player1 wins
+        if c == [2, 2, 2, 2]:
+            return False #False = player2 wins
---
+++ connect4.py
@@ -0,0 +1,78 @@
+import numpy as np
+import c4
+
+
+rowcount = 6
+colcount = 7
+
+gameover = False
+turn = 0
+
+board = c4.create_board(rowcount, colcount)
+
+print (f" {list(range(1, colcount+ 1))} \n  ------------------- \n {board}")
+
+print(f"Welcome to Connect4! You can choose to place your dot among the {colcount} spots on the board.\n The first player to connect 4 dots together wins! Enter 'exit' at anytime during the game to quit.")
+
+#creates filter that will recognize diagonal wins
+#left to right 
+
+
+#right to left
+
+
+
+while not gameover:
+	if turn % 2 == 0:
+		while True:
+			p1_input = input("Player 1, pick your spot!") #ask p1 input
+			try:
+				p1_input = int(p1_input)
+				p1_input -= 1
+				if p1_input >= colcount or p1_input < 0:
+					print("Please pick a number within the range of the board")
+					continue
+				else:
+					if not c4.is_valid_move(board, p1_input, rowcount):
+						print ("Please choose a valid move")
+						continue
+					else:
+						break
+			except ValueError:
+				print ("Please pick an integer")
+				continue
+		c4.drop_piece(board, p1_input, rowcount, 1)
+		print (f" {list(range(1, colcount+ 1))} \n  ------------------- \n {board}")
+
+	else:
+		while True:
+			p2_input = input("Player 2, pick your spot!") #ask p2 input
+			try:
+				p2_input = int(p2_input)
+				p2_input -= 1
+				if p2_input >= colcount or p2_input < 0:
+					print("Please pick a number within the range of the board")
+					continue
+				else:
+					if not c4.is_valid_move(board, p2_input, rowcount):
+						print ("Please choose a valid move")
+						continue
+					else:
+						break
+			except ValueError:
+				print ("Please pick an integer")
+				continue
+		c4.drop_piece(board, p2_input, rowcount, 2)
+		print (f" {list(range(1, colcount+ 1))} \n  ------------------- \n {board}")
+
+	win = c4.win_ver(board)
+
+	if win == None:
+		turn += 1
+	elif win:
+		print ("Player 1 won!")
+		break
+	else:
+		print ("Player 2 won!")
+		break
+	
